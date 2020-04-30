0e3ade4036c9ec8f27e3d01f6370409999265132
Dement0 <ilkeer.ozgur@gmail.com>
2020-04-30 16:39:09 +0200

    Add Regex for StringCalculator

--- src/main/kotlin/helpers/Helper.kt
+++ src/main/kotlin/helpers/Helper.kt
@@ -1,18 +1,20 @@
 package helpers
 
 fun leapYear(year: Int): Boolean {
-    if (year < 1580) throw IllegalArgumentException("First valid year should be 1580!")
-    else if (year % 100 == 0) {
-        return (year % 400 == 0)
-    } else return (year % 4 == 0)
+    return when {
+        year < 1580 -> throw IllegalArgumentException("First valid year should be 1580!")
+        year % 100 == 0 -> {
+            (year % 400 == 0)
+        }
+        else -> (year % 4 == 0)
+    }
 }
 
 fun addInt(numbers: String): Int? {
     return if (numbers.isBlank()) 0
     else {
-        numbers.toIntOrNull()
-        /*numbers.split(",")
+        return numbers.split(Regex("(,+)|(;+)"))
             .mapNotNull { it.toIntOrNull() }
-            .sum()*/
+            .sum()
     }
 }
--- src/test/kotlin/StringCalculatorTest.kt
+++ src/test/kotlin/StringCalculatorTest.kt
@@ -17,7 +17,7 @@ class StringCalculatorTest {
     fun `String returns null`() {
         val input = "Horse"
         val output = addInt(input)
-        assertThat(output).isNull()
+        assertThat(output).isEqualTo(0)
     }
 
     @Test
@@ -25,5 +25,47 @@ class StringCalculatorTest {
         val input = "22"
         val output = addInt(input)
         assertThat(output).isEqualTo(22)
+    }
+
+    @Test
+    fun `String of comma separated numbers returns Int`() {
+        val input = "2,5"
+        val output = addInt(input)
+        assertThat(output).isEqualTo(7)
+    }
+
+    @Test
+    fun `String of comma separated char and numbers returns Int`() {
+        val input = "2,a"
+        val output = addInt(input)
+        assertThat(output).isEqualTo(2)
+    }
+
+    @Test
+    fun `String of comma or semicolon separated numbers returns Int`() {
+        val input = "2;5"
+        val output = addInt(input)
+        assertThat(output).isEqualTo(7)
+    }
+
+    @Test
+    fun `String of comma or semicolon separated char and numbers returns Int`() {
+        val input = "5;a"
+        val output = addInt(input)
+        assertThat(output).isEqualTo(5)
+    }
+
+    @Test
+    fun `String of comma and semicolon separated numbers returns Int`() {
+        val input = "5,;7"
+        val output = addInt(input)
+        assertThat(output).isEqualTo(12)
+    }
+
+    @Test
+    fun `String of comma and semicolon separated char and numbers returns Int`() {
+        val input = "5,;a"
+        val output = addInt(input)
+        assertThat(output).isEqualTo(5)
     }
 }
