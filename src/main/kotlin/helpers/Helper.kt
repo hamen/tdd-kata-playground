0532558ae4396c27e727d91784d628e350377360
Dement0 <ilkeer.ozgur@gmail.com>
2020-04-30 12:42:53 +0200

    Write initial tests for StringCalculator

--- src/main/kotlin/helpers/Helper.kt
+++ src/main/kotlin/helpers/Helper.kt
@@ -1,9 +1,18 @@
 package helpers
 
-fun leapYear(year: Int) : Boolean {
-    if(year < 1580) throw IllegalArgumentException("First valid year should be 1580!")
-    else if(year %100 == 0) {
+fun leapYear(year: Int): Boolean {
+    if (year < 1580) throw IllegalArgumentException("First valid year should be 1580!")
+    else if (year % 100 == 0) {
         return (year % 400 == 0)
+    } else return (year % 4 == 0)
+}
+
+fun addInt(numbers: String): Int? {
+    return if (numbers.isBlank()) 0
+    else {
+        numbers.toIntOrNull()
+        /*numbers.split(",")
+            .mapNotNull { it.toIntOrNull() }
+            .sum()*/
     }
-    else return (year %4 == 0)
 }
--- src/test/kotlin/StringCalculatorTest.kt
+++ src/test/kotlin/StringCalculatorTest.kt
@@ -1,9 +1,29 @@
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNull
+import helpers.addInt
 import org.junit.jupiter.api.Test
 
 class StringCalculatorTest {
 
     @Test
-    fun `foo`() {
+    fun `Empty input returns 0`() {
+        val input = ""
+        val output = addInt(input)
+        assertThat(output).isEqualTo(0)
+    }
 
+    @Test
+    fun `String returns null`() {
+        val input = "Horse"
+        val output = addInt(input)
+        assertThat(output).isNull()
+    }
+
+    @Test
+    fun `String of number returns Int`() {
+        val input = "22"
+        val output = addInt(input)
+        assertThat(output).isEqualTo(22)
     }
 }
